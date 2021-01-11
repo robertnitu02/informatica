40c95bd0cbdb6b169c92dc713830605decce3242
niturobert02 <niturobert02@yahoo.com>
2021-01-11 22:11:31 +0200

    Adaugata varianta 2014 fara S3 ex 3&4

--- Variante Bac/compilator/skema/main.cpp
+++ Variante Bac/compilator/skema/main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
-#include <fstream>
-#include <math.h>
 
 using namespace std;
 
-ofstream out("BAC.txt");
-
 int main() {
-    int x;
-    cin >> x;
-
     return 0;
 }
---
+++ Variante Bac/varianta_2014/Subiectul II/exercitiul 5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m, n, mat[51][51];
+    cin >> m >> n;
+    for(int i = 1; i <= m; i++)
+        for(int j = 1; j <= n; j++)
+            cin >> mat[i][j];
+    int linie = m - 1;
+    int coloana = n - 1;
+    for(int i = linie; i < m; i++) {
+        for(int j = 1; j <= n; j++)
+            mat[i][j] = mat[i+1][j];
+    }
+    for(int i = 1; i < m; i++) {
+        for(int j = coloana; j < n; j++)
+            mat[i][j] = mat[i][j+1];
+    }
+    m--; n--;
+    for(int i = 1; i <= m; i++) {
+        for(int j = 1; j <= n; j++) {
+            cout << mat[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
