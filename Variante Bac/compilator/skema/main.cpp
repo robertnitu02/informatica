ff731dbdb074f3980f7af83a8a935d444fd46675
niturobert02 <niturobert02@yahoo.com>
2021-01-12 12:47:23 +0200

    Fix varianta 2014 ex 5

--- Variante Bac/compilator/skema/main.cpp
+++ Variante Bac/compilator/skema/main.cpp
@@ -3,5 +3,27 @@
 using namespace std;
 
 int main() {
+    unsigned int m, n, mat[51][51];
+    cin >> m >> n;
+    for(int i = 1; i <= m; i++)
+        for(int j = 1; j <= n; j++)
+            cin >> mat[i][j];
+    unsigned int linie = m - 1;
+    unsigned int coloana = n - 1;
+    for(int i = linie; i < m; i++) {
+        for(int j = 1; j <= n; j++)
+            mat[i][j] = mat[i+1][j];
+    }
+    for(int i = 1; i < m; i++) {
+        for(int j = coloana; j < n; j++)
+            mat[i][j] = mat[i][j+1];
+    }
+    m--; n--;
+    for(int i = 1; i <= m; i++) {
+        for(int j = 1; j <= n; j++) {
+            cout << mat[i][j] << ' ';
+        }
+        cout << endl;
+    }
     return 0;
 }
--- Variante Bac/varianta_2014/Subiectul II/exercitiul 5.cpp
+++ Variante Bac/varianta_2014/Subiectul II/exercitiul 5.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main() {
-    int m, n, mat[51][51];
+    unsigned int m, n, mat[51][51];
     cin >> m >> n;
     for(int i = 1; i <= m; i++)
         for(int j = 1; j <= n; j++)
             cin >> mat[i][j];
-    int linie = m - 1;
-    int coloana = n - 1;
+    unsigned int linie = m - 1;
+    unsigned int coloana = n - 1;
     for(int i = linie; i < m; i++) {
         for(int j = 1; j <= n; j++)
             mat[i][j] = mat[i+1][j];
