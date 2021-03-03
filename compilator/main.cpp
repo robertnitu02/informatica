cd56377dd1c0b47205a00ddd596ee7c8ac0fe311
Robert Nițu <niturobert02@yahoo.com>
2021-03-03 11:51:25 +0200

    Adaugata varianta din 2021 - martie I

--- compilator/main.cpp
+++ compilator/main.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
+unsigned int factori(unsigned int n, unsigned int m) {
+    unsigned int val = 1;
+    unsigned int divizorN = 2, divizorM = 2;
+    int putereN, putereM;
+    while(n > 1 && m > 1) {
+        putereN = 0;
+        putereM = 0;
+        while(n % divizorN == 0) {
+            putereN++;
+            n = n / divizorN;
+        }
+        while(m % divizorM == 0) {
+            putereM++;
+            m = m / divizorM;
+        }
+        divizorN++;
+        divizorM++;
+        if (putereN == putereM)
+            val++;
+    }
+    return val - 1;
+}
+
 int main() {
-    cout << "Variante Informatica - Robert Nitu" << endl;
+    cout << factori(16500, 10780);
     return 0;
 }
