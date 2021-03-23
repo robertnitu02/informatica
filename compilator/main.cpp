4b4ef3581d94b33fef608e83b7c00eb8cc86821f
Mentosan <niturobert02@yahoo.com>
2021-03-23 18:43:17 +0200

    webinar

--- compilator/main.cpp
+++ compilator/main.cpp
@@ -3,26 +3,6 @@
 using namespace std;
 
 int main() {
-    unsigned int n;
-    int x, m, p, cn;
 
-    cin >> n;
-    x = 0;
-    m = 0;
-    p = 1;
-
-    while(x < 10) {
-        cn = n;
-        while(cn != 0) {
-            int c = cn % 10;
-            cn = cn / 10;
-            if(c == x) {
-                m = c * p + m;
-                p = p * 10;
-            }
-        }
-        x += 2;
-    }
-    cout << m;
     return 0;
 }
