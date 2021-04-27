1712058bfd2ac14b97e44b0f856ec3a80c184fb3
Mentosan <niturobert02@yahoo.com>
2021-04-27 19:44:01 +0300

    adaugata prima parte din testul de antrenament 4

--- compilator/main.cpp
+++ compilator/main.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
-int main() {
+int f(int n) {
+    int c;
+    if(n == 0) return 9;
+    c = f(n / 10);
+    if(n % 10 < c)
+        return n%10;
+    return c;
+}
 
+int main() {
+    cout << f(38627);
     return 0;
 }
