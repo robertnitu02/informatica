5b24b048789c3ab961dc4a64d0858a05f85c2e3a
Mentosan <niturobert02@yahoo.com>
2021-06-17 19:40:00 +0300

    update

--- compilator/main.cpp
+++ compilator/main.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
-#include <fstream>
 
 using namespace std;
 
-ifstream in("bac.txt");
-
 int main() {
-    int nr, val = -1;
-    int pozitie = 1;
-
-    in >> nr;
-
-    while(!in.eof()) {
-        if (val == -1) {
-            val = nr;
-        } else {
-            if(nr >= val)
-                pozitie++;
-        }
-        in >> nr;
-    }
-
-    in.close();
-    cout << pozitie;
     return 0;
 }
